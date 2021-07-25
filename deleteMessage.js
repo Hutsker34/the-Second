fb7e62fcce08aec924fd7f585196fedc314b51bf
viacheslav34 <77201329+viacheslav34@users.noreply.github.com>
2021-07-24 18:56:23 -0700

    сделали удаление сообщений

--- deleteMessage.js
+++ deleteMessage.js
@@ -5,23 +5,43 @@ function deleteMessage () {
   del.addEventListener('dblclick', function (e) {
     const target = e.target.closest('.del__message')
     const conf = target && confirm('вы точно хотите удалить сообщение')
-    if (conf) {
-      target.remove()
-      console.log(historySettings.historyID, 'delete')
-      historySettings.history[historySettings.historyID] = historySettings.history[historySettings.historyID].filter(item => item.id !== target.id)
-
-      fetch('http://localhost:8000/user', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          history: historySettings.history,
-          id: historySettings.historyID
-        })
-      })
+    if (!conf) {
+      return
     }
+
+    fetch('http://localhost:8000/delete-mess', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        history: historySettings.historyID,
+        id: target.id
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (res.success) {
+          target.remove()
+        } else {
+          alert('невозможно удолить сообщение!')
+        }
+      })
+
+    historySettings.history[historySettings.historyID] = historySettings.history[historySettings.historyID].filter(item => item.id !== target.id)
+
+    fetch('http://localhost:8000/get-dialogue', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        history: historySettings.history,
+        id: historySettings.historyID
+      })
+    })
   })
 }
 export {
